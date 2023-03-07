5546a0ec6b768f06b2f9ea0819c7f96c494bcf07
zennky <erick.ibanez.velasco@gmail.com>
2023-03-06 23:05:47 -0400

    proyecto inicial de java 17

---
+++ academy-notes/src/main/java/com/academia/AcademyNotesApplication.java
@@ -0,0 +1,13 @@
+package com.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyNotesApplication.class, args);
+	}
+
+}
